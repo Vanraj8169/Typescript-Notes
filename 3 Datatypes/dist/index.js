29bc6a054ac9023f61f47c31c31ef1abff458466
Vanraj Pardeshi <truecapturers@gmail.com>
2024-09-14 16:56:07 +0530

    Objects

--- 3 Datatypes/dist/index.js
+++ 3 Datatypes/dist/index.js
@@ -1,11 +1,11 @@
 "use strict";
-let a = "234";
-let num = 234;
-let check;
-check = false;
+// let a: string = "234";
+// let num: number = 234;
+// let check: boolean;
+// check = false;
 // union operator
-let surname;
-surname = "5342";
+// let surname: string | number;
+// surname = "5342";
 // function
 // const func=  (n:number,m:number):number => {
 //     console.log(n,m);
@@ -18,3 +18,11 @@ surname = "5342";
 // };
 // const arr: number[] = [123, 42, 34, 234, 234];
 // const arr: Array<number | string> = [1234,234,234,234,234];
+// Objects
+// const obj: {
+//   height: number;
+//   weight: number;
+// } = {
+//   height: 234,
+//   weight: 23,
+// };
--- 3 Datatypes/src/index.ts
+++ 3 Datatypes/src/index.ts
@@ -1,11 +1,11 @@
-let a: string = "234";
-let num: number = 234;
-let check: boolean;
-check = false;
+// let a: string = "234";
+// let num: number = 234;
+// let check: boolean;
+// check = false;
 
 // union operator
-let surname: string | number;
-surname = "5342";
+// let surname: string | number;
+// surname = "5342";
 
 // function
 // const func=  (n:number,m:number):number => {
@@ -23,25 +23,14 @@ surname = "5342";
 // const arr: number[] = [123, 42, 34, 234, 234];
 // const arr: Array<number | string> = [1234,234,234,234,234];
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Objects
+// const obj: {
+//   height: number;
+//   weight: number;
+// } = {
+//   height: 234,
+//   weight: 23,
+// };
 
 
 
