9d34652aa10bd85acc50354f547f019dd5eb47f1
Vanraj Pardeshi <truecapturers@gmail.com>
2024-09-14 16:05:10 +0530

    01 Basic

---
+++ 1 Basic/index.js
@@ -0,0 +1,5 @@
+var a = "nice";
+console.log(a);
+var b = 44;
+console.log(b);
+// tsc is used to compile typescript code
---
+++ 1 Basic/index.ts
@@ -0,0 +1,7 @@
+const a:String = "nice";
+console.log(a);
+
+const b:number = 44;
+console.log(b);
+
+// tsc is used to compile typescript code
