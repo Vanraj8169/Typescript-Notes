773ad745883004736eb6fdeb6f4a0933d0f2f7e0
Vanraj Pardeshi <truecapturers@gmail.com>
2024-09-14 16:34:43 +0530

    3 Data Structure

---
+++ 3 Datatypes/dist/index.js
@@ -0,0 +1,20 @@
+"use strict";
+let a = "234";
+let num = 234;
+let check;
+check = false;
+// union operator
+let surname;
+surname = "5342";
+// function
+// const func=  (n:number,m:number):number => {
+//     console.log(n,m);
+//     return n+m;
+// }
+// type aliases
+// type Username = (n: string, m: string) => string;
+// const func: Username = (n, m) => {
+//   return n + m;
+// };
+// const arr: number[] = [123, 42, 34, 234, 234];
+// const arr: Array<number | string> = [1234,234,234,234,234];
---
+++ 3 Datatypes/src/index.ts
@@ -0,0 +1,47 @@
+let a: string = "234";
+let num: number = 234;
+let check: boolean;
+check = false;
+
+// union operator
+let surname: string | number;
+surname = "5342";
+
+// function
+// const func=  (n:number,m:number):number => {
+//     console.log(n,m);
+//     return n+m;
+// }
+
+// type aliases
+// type Username = (n: string, m: string) => string;
+
+// const func: Username = (n, m) => {
+//   return n + m;
+// };
+
+// const arr: number[] = [123, 42, 34, 234, 234];
+// const arr: Array<number | string> = [1234,234,234,234,234];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
