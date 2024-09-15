594b869a5a73b0518e3babd04a30242b8ef1cc3d
Vanraj Pardeshi <truecapturers@gmail.com>
2024-09-15 18:28:11 +0530

    8 Generics

---
+++ 8 Generics/src/index.ts
@@ -0,0 +1,19 @@
+// Generics
+// const func = <T>(n: T): T => {
+//   return n;
+// };
+
+// console.log(func("hello"));
+// console.log(func(3));
+
+// type PersonType = {
+//   name: string;
+//   age: number;
+// };
+
+// const person: PersonType = {
+//   name: "Vanraj",
+//   age: 23,
+// };
+
+// const personFunc = func<PersonType>(person);
